c13112f55c6afbd518c35121b3c367cd7d93cbf9
Benjamin Böcker <benjamin.boecker@palasthotel.de>
2021-07-02 09:21:14 +0200

    Added convenience initializer and accessor for UIEdgeInsets

--- Sources/PHUIKitExtensions/UIEdgeInsets.swift
+++ Sources/PHUIKitExtensions/UIEdgeInsets.swift
@@ -18,4 +18,27 @@ public extension UIEdgeInsets {
 	init(top: CGFloat = 0.0, leading: CGFloat = 0.0, bottom: CGFloat = 0.0, trailing: CGFloat = 0.0) {
 		self.init(top: top, left: leading, bottom: bottom, right: trailing)
 	}
+	
+	/// Convenience initializer that provides default values of `0.0` for values not set.
+	/// - Parameters:
+	///   - vertical: The vertical inset, setting `.top` and `.bottom` to this value.
+	///   - horizontal: The horizontal inset, setting `.leading` and `.trailing` to this value.
+	init(vertical: CGFloat = 0.0, horizontal: CGFloat = 0.0) {
+		self.init(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
+	}
+	
+	
+	/// Convenience accessor to create a `UIEdgeInsets` value with vertical insets.
+	/// - Parameter value: The vertical inset, setting `.top` and `.bottom` to this value.
+	/// - Returns: A `UIEdgeInsets` value, with `.top` and `.bottom` set to the given parameter, and `.leading` and `.trailing` set to `0`.
+	static func vertical(_ value: CGFloat) -> UIEdgeInsets {
+		UIEdgeInsets(vertical: value)
+	}
+	
+	/// Convenience accessor to create a `UIEdgeInsets` value with horizontal insets.
+	/// - Parameter value: The horizontal inset, setting `.leading` and `.trailing` to this value.
+	/// - Returns: A `UIEdgeInsets` value, with `.leading` and `.trailing` set to the given parameter, and `.top` and `.bottom` set to `0`.
+	static func horizontal(_ value: CGFloat) -> UIEdgeInsets {
+		UIEdgeInsets(horizontal: value)
+	}
 }
