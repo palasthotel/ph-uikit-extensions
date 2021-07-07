9327e0c1758999dc54d82259a8ddb082a96dd6dc
Benjamin Böcker <benjamin.boecker@palasthotel.de>
2021-07-07 11:23:49 +0200

    UIColor hex string initializer

--- Sources/PHUIKitExtensions/UIColor.swift
+++ Sources/PHUIKitExtensions/UIColor.swift
@@ -10,6 +10,29 @@ import UIKit
 
 
 public extension UIColor {
+	
+	convenience init?(hex: String) {
+		var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+		if (cString.hasPrefix("#")) {
+			cString.remove(at: cString.startIndex)
+		}
+
+		if ((cString.count) != 6) {
+			return nil
+		}
+
+		var rgbValue:UInt64 = 0
+		Scanner(string: cString).scanHexInt64(&rgbValue)
+
+		self.init(
+			red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+			green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+			blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+			alpha: CGFloat(1.0)
+		)
+	}
+
 	convenience init(hex: Int, alpha: CGFloat = 1.0) {
 		let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
 		let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
