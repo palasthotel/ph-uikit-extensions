bb8ef8c951dda1bb72115f7ccd293647d4eeb287
Benjamin Böcker <benjamin.boecker@palasthotel.de>
2021-09-08 16:50:22 +0200

    Refactored and fixed the preferredFont function of UIFont

--- Sources/PHUIKitExtensions/UIFont.swift
+++ Sources/PHUIKitExtensions/UIFont.swift
@@ -15,14 +15,39 @@ public extension UIFont {
 	///   - style: The style of the font. Default value is `.body`.
 	///   - weight: The weight of the font. Default value is `.regular`.
 	///   - design: The design of the font. Default value is `.default`.
+	///   - symbolicTraits: Optional symbolic traits of the font, not all traits work with all fonts. Default value is `[]`.
 	/// - Returns: An instance of `UIFont`, using the given style, weight and design.
-	static func preferredFont(for style: TextStyle = .body, weight: Weight = .regular, design: UIFontDescriptor.SystemDesign = .default) -> UIFont {
-		let fontSize = UIFont.preferredFont(forTextStyle: style).pointSize
-		if let descriptor = UIFont.systemFont(ofSize: fontSize, weight: weight).fontDescriptor.withDesign(design) {
-			return UIFont(descriptor: descriptor, size: fontSize)
+	static func preferredFont(
+		for style: TextStyle = .body,
+		weight: Weight = .regular,
+		design: UIFontDescriptor.SystemDesign = .default,
+		symbolicTraits: UIFontDescriptor.SymbolicTraits = []
+	) -> UIFont {
+		if let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+			.withDesign(design)?
+			.withSymbolicTraits(symbolicTraits)
+		{
+			return UIFont(descriptor: descriptor, size: 0)
+				.withWeight(weight)
 		} else {
 			return UIFont.preferredFont(forTextStyle: style)
 		}
 	}
+	
+	private func withWeight(_ weight: UIFont.Weight) -> UIFont {
+		var attributes = fontDescriptor.fontAttributes
+		var traits = (attributes[.traits] as? [UIFontDescriptor.TraitKey: Any]) ?? [:]
+
+		traits[.weight] = weight
+
+		attributes[.name] = nil
+		attributes[.traits] = traits
+		attributes[.family] = familyName
+
+		let descriptor = UIFontDescriptor(fontAttributes: attributes)
+
+		return UIFont(descriptor: descriptor, size: pointSize)
+	}
+
 
 }
