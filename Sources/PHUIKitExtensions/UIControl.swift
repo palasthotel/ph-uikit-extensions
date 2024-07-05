3fb6e9828f6da64e5bb412751e59ed709ed0b616
Ben Böcker <benjamin.boecker@palasthotel.de>
2024-07-05 11:57:35 +0200

    made the uicontrol function open for subclassing

--- Sources/PHUIKitExtensions/UIControl.swift
+++ Sources/PHUIKitExtensions/UIControl.swift
@@ -9,11 +9,11 @@ import Foundation
 import UIKit
 
 
-public extension UIControl {
+extension UIControl {
 	
 	/// Adds an action closure as the `.touchUpInside` action to this button.
 	/// - Parameter action: The action that is called when the button is pressed.
-	func addAction(_ action: @escaping () -> Void) {
+	@objc open func addAction(_ action: @escaping () -> Void) {
 		self.addAction(UIAction { _ in
 			action()
 		}, for: .touchUpInside)
